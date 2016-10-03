1e1d05190d2886e32fcdda22eaa541d4aaf51471
Brenton Salmi <brent@faradayrf.com>
2016-10-03 00:28:00 -0700

    Basic message application working in bare bones sending about 1-2kbps using the super janky and underutilizing setup, I'm committing pyc files, I'm sorry!

--- Tutorials/FaradayIO/Message/Basic_MSG_App/Test_Rx.py
+++ Tutorials/FaradayIO/Message/Basic_MSG_App/Test_Rx.py
@@ -5,8 +5,5 @@ faraday_rx_msg_object = faraday_msg.message_app_Rx()
 faraday_rx_msg_sm = faraday_msg.Msg_State_Machine_Tx()
 faraday_io = faradaybasicproxyio.proxyio(80)
 
-
-
-
-while(1):
+while 1:
     faraday_rx_msg_object.RxMsgLoop()
