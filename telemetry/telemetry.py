79fd07dcbbfea8a955c4522db1da7dbe2f45d813
kb1lqc <bryce@faradayrf.com>
2016-10-16 00:36:10 -0700

    Cleaned up code and commented. Still needs some cleanign up like making more functions to clean up the actual flask functions.

--- telemetry/telemetry.py
+++ telemetry/telemetry.py
@@ -106,7 +106,7 @@ def telemetry_worker(config):
 
                     else:
 
-                        sqlInsert(dbFilename,parsedTelemetry[0])
+                        sqlInsert(parsedTelemetry[0])
                         telemetryDicts[str(callsign) + str(nodeid)].append(parsedTelemetry[1])
 
          time.sleep(1) # should slow down
@@ -129,16 +129,14 @@ def dbTelemetry():
         callsign = request.args.get("callsign", "%")
         nodeid = request.args.get("nodeid", "%")
         direction = request.args.get("direction", 0)
-        #limit = request.args.get("limit", 100)
         startTime = request.args.get("starttime", None)
         endTime = request.args.get("endtime", None)
         timespan = request.args.get("timespan", 5*60)
 
-        #limit = str(limit)
         nodeid = str(nodeid)
         direction = int(direction)
         callsign = str(callsign).upper()
-
+        timespan = int(timespan)
 
     except ValueError as e:
         logger.error("ValueError: " + str(e))
@@ -150,6 +148,11 @@ def dbTelemetry():
         logger.error("KeyError: " + str(e))
         return json.dumps({"error": str(e)}), 400
 
+    # Validate timespan
+    if timespan <= 0:
+        message = "Error: Timespan '{0}' is invalid".format(timespan)
+        return json.dumps({"error": message}), 400
+
     # Create tuple of parameters for SQLite3
     parameters = {}
     parameters["CALLSIGN"] = callsign
@@ -159,9 +162,15 @@ def dbTelemetry():
     parameters["ENDTIME"] = endTime
     parameters["TIMESPAN"] = timespan
 
-    queryResults = queryDb(parameters)
+    data = []
+    data = queryDb(parameters)
 
-    return json.dumps(queryResults, indent=1), 200,\
+    # Check if data returned, if not, return HTTP 204
+    if len(data) <= 0:
+        logger.info("No station data in last %d seconds", timespan)
+        return '', 204  # HTTP 204 response cannot have message data
+
+    return json.dumps(data, indent=1), 200,\
             {'Content-Type': 'application/json'}
 
 @app.route('/raw', methods=['GET'])
@@ -169,15 +178,16 @@ def rawTelemetry():
     """
     Provides a RESTful interface to the decoded raw telemetry at URL '/raw'
 
-    Starts a flask server on port 8001 (default) which serves data from the
-    requested Faraday via it's proxy interface on localhost URL "/". Callsign
-    and nodeid of the Faraday radio connected via USB is required.
+    The rawTelemetry() function is run when the URL "/raw" is queried. It
+    provides non-SQLite database dequeu results. Each query can pop data off of
+    a queu, thus ensuring there are no duplicates and acting like a proxy with
+    decoded data instead of BASE64 encoded data.
     """
 
     try:
         # Obtain URL parameters
-        callsign = request.args.get("callsign")
-        nodeId = request.args.get("nodeid")
+        callsign = request.args.get("callsign", None)
+        nodeId = request.args.get("nodeid", None)
         limit = request.args.get("limit", None)
 
     except ValueError as e:
@@ -190,25 +200,25 @@ def rawTelemetry():
         logger.error("KeyError: " + str(e))
         return json.dumps({"error": str(e)}), 400
 
-
-
-
-
     # Check to see that required parameters are present
+    # Use try statement to fail cleanly with relevant error information
     try:
+        # If callsign is present then nodeid is required
         if callsign is not None:
             if nodeId is None:
-                callsign = str(callsign).upper()
-                # Required if callsing is present
+
                 raise StandardError("Missing 'nodeid' parameter")
             else:
+                # Convert nodeId to int and callsign to uppercase string
                 nodeId = int(nodeId)
+                callsign = str(callsign).upper()
+
                 # Check to see if the Node ID is in the valid range
                 if nodeId > 255 or nodeId < 0:
                     raise ValueError(
                         "Faraday Node ID's valid integer between 0-255")
         else:
-            # Don't change anythin
+            # Don't change anything since callsign is None
            pass
 
         if limit is None:
@@ -217,10 +227,9 @@ def rawTelemetry():
             for key, value in telemetryDicts.iteritems():
                 temp.append(len(value))
             limit = int(max(temp))
-
         else:
+            # Limit provided, convert to int and check if it's valid
             limit = int(limit)
-            # Check for less than or equal to zero case
             if limit <= 0:
                 message = "Error: Limit '{0}' is invalid".format(limit)
                 return json.dumps({"error": message}), 400
@@ -238,36 +247,54 @@ def rawTelemetry():
         logger.error("StandardError: " + str(e))
         return json.dumps({"error": str(e)}), 400
 
-    # Pop data off of the queue
+    # Ready to get data from the queue. We have to cases: callsign and nodeid
+    # are present or they are not. The resulting dictionary created needs to
+    # be different in both cases. Use try statement to fail cleanly
     try:
+        # Clear all data from any previous requests
         data = []
+
+        # If callsign and nodeId are not present, return all connected radio
+        # queue data.
         if callsign == None and nodeId == None:
+            # Iterate through each faraday radio connected via USB
             for key, value in telemetryDicts.iteritems():
+                # Make sure queue actually has data in it
                 if (len(value) > 0):
                     stationData = []
                     station = {}
                     while value:
+                        # While data is still in the queue pop off items
+                        # Pops off right to maintain newest item in object 0
                         packet = []
                         packet = value.pop()
                         if len(stationData) >= limit:
+                            # Hit the limit, break out of the while loop early
                             break
+                        # Append packet to stationData list
                         stationData.append(packet)
+                    # All necessary data from radio obtained, add to dictionary
                     station[key] = stationData
+            # The data list is a list of dictionaries for json.dumps()
             data.append(station)
         else:
-            print callsign, nodeId
+            # Local radio has been specified, only return data from it
             stationData = []
             station = {}
             if (len(telemetryDicts[str(callsign) + str(nodeId)]) > 0):
+                # Make sure specific radio queue actually has data in it.
+                # If so, pop off the items and build up a dictionary for it.
                 while telemetryDicts[str(callsign) + str(nodeId)]:
+                    # Pop off items from right so JSON has newest items first
                     packet = \
                         telemetryDicts[
-                            str(callsign) + str(nodeId)].pop()  # JSON latest first
+                            str(callsign) + str(nodeId)].pop()
                     stationData.append(packet)
-                    print "LEN ", len(stationData)
                     station[str(callsign) + str(nodeId)] = stationData
                     if len(stationData) >= limit:
+                        # Hit the limit, break from loop
                         break
+                # Append specified radio data to the json.dumps() list
                 data.append(station)
 
     except ValueError as e:
@@ -283,6 +310,7 @@ def rawTelemetry():
         logger.error("StandardError: " + str(e))
         return json.dumps({"error": str(e)}), 400
 
+    # Completed our query for "/raw", return json.dumos() and HTTP 200
     return json.dumps(data, indent=1), 200,\
             {'Content-Type': 'application/json'}
 
@@ -291,8 +319,11 @@ def stations():
     """
     Provides a RESTful interface to station queries at URL '/stations'
 
-    Starts a flask server on port 8001 (default) which serves data from the
-    requested Faraday via it's proxy interface on localhost URL "/stations".
+    This function, stations(), runs whenever "/stations" URL is queried. The
+    intent of this function is to return a JSON dictionary of stations and
+    the time they were heard. If no timespan or range is specified then it
+    defaults to the last 5 minutes. If a specific station is specified, then
+    the last time it was heard is returned.
     """
 
     try:
@@ -303,6 +334,7 @@ def stations():
         callsign = request.args.get("callsign", None)
         nodeId = request.args.get("nodeid", "%")
 
+        # Timespan will allways be an integer
         timespan = int(timespan)
 
     except ValueError as e:
@@ -318,6 +350,7 @@ def stations():
         logger.error("StandardError: " + str(e))
         return json.dumps({"error": str(e)}), 400
 
+    # Clear parameters dictionary and add URL parameters to it
     parameters = {}
     parameters["TIMESPAN"] = timespan
     parameters["STARTTIME"] = startTime
@@ -325,19 +358,30 @@ def stations():
     parameters["CALLSIGN"] = callsign
     parameters["NODEID"] = nodeId
 
+    # Provide parameters to queryStationsDb to return the result SQLite rows
     data = queryStationsDb(parameters)
 
+    # Check if no stations returned, if not, return HTTP 204
+    if len(data) <= 0:
+        logger.info("Station(s) not heard in last %d seconds", timespan)
+        return '', 204  # HTTP 204 response cannot have message data
+
+    # Completed the /stations request, return data json.dumps() and HTTP 200
+    print len(data)
     return json.dumps(data, indent=1), 200,\
             {'Content-Type': 'application/json'}
 
 @app.errorhandler(404)
 def pageNotFound(error):
     """HTTP 404 response for incorrect URL"""
+
+    # Completed handling of unknown URL, return json error message and HTTP 404
     logger.error("Error: " + str(error))
     return json.dumps({"error": "HTTP " + str(error)}), 404
 
 # Database Functions
 def initDB():
+    """Initialize database, if not present then create it"""
     # Obtain configuration filenames
     dbFilename = telemetryConfig.get("database", "filename")
     dbSchema = telemetryConfig.get("database", "schemaname")
@@ -346,28 +390,35 @@ def initDB():
     if os.path.isfile(dbFilename):
         pass
     else:
-        # Open database schema SQL file and execute the SQL
+        # Open database schema SQL file and execute the SQL functions inside
+        # after connecting. Close the database when complete.
         with open(dbSchema, 'rt') as f:
             schema = f.read()
+
         conn = sqlite3.connect(dbFilename)
         cur = conn.cursor()
         cur.executescript(schema)
+
         conn.close()
 
-def sqlInsert(db, data):
-    """Takes in database filename and data tuple and inserts data into table"""
-    data = (None,) + data  # Add a null to tuple for KEYID
+def sqlInsert(data):
+    """Takes in a data tuple and inserts int into the telemetry SQLite table"""
 
-    # Create parameter substitute "?" string for SQL query
+    # Read in name of telemetry databse
+    db = telemetryConfig.get("database", "filename")
+
+    # Create parameter substitute "?" string for SQL query then create SQL
+    data = (None,) + data  # Add a null to tuple for KEYID
     numKeys = len(data)
     paramSubs = "?" * (numKeys)
     paramSubs = ",".join(paramSubs)
+    sql = "INSERT INTO TELEMETRY VALUES(" + paramSubs + ")"
 
     # Connect to database, create SQL query, execute query, and close database
     try:
         conn = sqlite3.connect(db)
         cursor = conn.cursor()
-        sql = "INSERT INTO TELEMETRY VALUES(" + paramSubs + ")"
+
         # Use connection as context manager to rollback automatically if error
         with conn:
             conn.execute(sql,data)
@@ -381,29 +432,41 @@ def sqlInsert(db, data):
     except KeyError as e:
         logger.error("KeyError: " + str(e))
 
-    # Completed INSERT, close database
+    # Completed, close database
     conn.close()
 
 def queryDb(parameters):
-    """Takes in parameters to query the SQLite database, returns the results"""
-    print "PARAMS: ", parameters
-    timeTuple = generateStartStopTimes(parameters)
+    """
+    Takes in parameters to query the SQLite database, returns the results
 
+    Performs a SQL query to retrieve data from specific times, stations, or
+    ranges of time. Returns all results as a list of JSON dictionaries
+    """
+    # Use supplied parameters to generate a Tuple of epoch start/stop times
+    # SQLite3 parameters need to be Tuples
+    timeTuple = generateStartStopTimes(parameters)
+    callsign = parameters["CALLSIGN"].upper()
+    nodeid = parameters["NODEID"]
+    paramTuple = (callsign, nodeid) + timeTuple
+
+    # Detect the direction, this will change the query from searching for
+    # the source or destination radio. Must generate two slightly different
+    # SQL queries for each case
     if parameters["DIRECTION"] == 0:
-        print "source"
+        # Direction 0 = Source radio
         sqlWhereCall = "WHERE SOURCECALLSIGN LIKE ? "
         sqlWhereID = "AND SOURCEID LIKE ? "
-        sqlEpoch ="AND EPOCH BETWEEN ? AND ? "
-
     else:
-        print "dest"
+        # Direction 1 = Destination radio
         sqlWhereCall = "WHERE DESTINATIONCALLSIGN LIKE ? "
         sqlWhereID = "AND DESTINATIONID LIKE ? "
-        sqlEpoch ="AND EPOCH BETWEEN ? AND ? "
 
+    sqlBeg = "SELECT * FROM TELEMETRY "
+    sqlEpoch ="AND EPOCH BETWEEN ? AND ? "
+    sqlEnd = "ORDER BY KEYID DESC"
 
-    # Initialize variables
-    results = []
+    # Create  SQL Query string
+    sql = sqlBeg + sqlWhereCall + sqlWhereID + sqlEpoch + sqlEnd
 
     # Open configuration file
     dbFilename = telemetryConfig.get("database", "filename")
@@ -411,7 +474,10 @@ def queryDb(parameters):
     # Connect to database, create SQL query, execute query, and close database
     try:
         conn = sqlite3.connect(dbFilename)
-        conn.row_factory = sqlite3.Row
+        conn.row_factory = sqlite3.Row  # Row_factory returns column/values
+        cur = conn.cursor()
+        cur.execute(sql,paramTuple)
+        rows = cur.fetchall()
 
     except StandardError as e:
         logger.error("StandardError: " + str(e))
@@ -422,17 +488,9 @@ def queryDb(parameters):
     except KeyError as e:
         logger.error("KeyError: " + str(e))
 
-    cur = conn.cursor()
-    sqlBeg = "SELECT * FROM TELEMETRY "
-    sqlEnd = "ORDER BY KEYID DESC"
-    sql = sqlBeg + sqlWhereCall + sqlWhereID + sqlEpoch + sqlEnd
-    print sql
-    #parameters["LIMIT"] = 5
-    #print parameters["LIMIT"]
-    try:
-        cur.execute(sql,(parameters["CALLSIGN"].upper(),parameters["NODEID"], str(timeTuple[0]), str(timeTuple[1])))
-        rows = cur.fetchall()
 
+    # Iterate through resulting data and create a list of dictionaries for JSON
+    try:
         sqlData = []
         for row in rows:
             rowData = {}
@@ -449,15 +507,20 @@ def queryDb(parameters):
     except KeyError as e:
         logger.error("KeyError: " + str(e))
 
-    # Completed query, close database
+    # Completed query, close database, return sqlData list of dictionaries
     conn.close()
-
     return sqlData
 
 def queryStationsDb(parameters):
-    """Takes in parameters to query the SQLite database, returns the results"""
-    print parameters
+    """
+    Takes in parameters to query the SQLite database, returns the results
 
+    Performs a SQL query to retrieve data about stations in the SQLite db.
+    Can retrieve all stations ever heard, in a specific time range, or in
+    a timespan before now. Returns all results as a list of JSON dictionaries
+    """
+
+    # Check for whether a time range or timespan is being specified
     if parameters["STARTTIME"] != None and parameters["ENDTIME"] != None:
         # Start end end times provided, ignore timespan
         startTime = str(parameters["STARTTIME"])
@@ -466,39 +529,44 @@ def queryStationsDb(parameters):
     else:
         # We should use the timespan provided to generate start and stop times
         endEpoch = time.time()
-        startEpoch = endEpoch - float(parameters["TIMESPAN"])
+        startEpoch = endEpoch - int(parameters["TIMESPAN"])
         timeTuple = (startEpoch, endEpoch)
 
+    # Specify and create SQL command string
     sqlBeg = "SELECT SOURCECALLSIGN, SOURCEID, EPOCH FROM TELEMETRY "
     sqlWhere = "WHERE EPOCH BETWEEN ? AND ? "
     sqlEnd = "GROUP BY SOURCECALLSIGN, SOURCEID ORDER BY EPOCH DESC"
 
     # detect if callsign/nodeid provided, return the last time it was heard
     if parameters["CALLSIGN"] != None:
-        print "limit to one!"
+        # Since a callsign was specified, simply search the entire db for it
+        # and return the last epoch time it was heard.
         timeTuple = (0, time.time())
+
+        # Update the sqlWhere and sqlEnd strings for this query
         sqlWhere = sqlWhere + "AND SOURCECALLSIGN LIKE ? AND SOURCEID LIKE ? "
         sqlEnd = sqlEnd + " LIMIT 1"
+
+        # Create paramTuple for SQLite3 execute function
         paramTuple = timeTuple + (str(parameters["CALLSIGN"]), parameters["NODEID"])
-        print paramTuple
     else:
+        # No callsign was provided, keep SQL query as defined
+        # Create paramTuple from only timeTuple
         paramTuple = timeTuple
 
-    # Initialize variables
-    results = []
-    currentTime = time.time()
-    try:
-        startTime = float(currentTime) - float(parameters["TIMESPAN"])
-    except:
-        print "fail"
+    # Create SQL query string
+    sql = sqlBeg + sqlWhere + sqlEnd
 
-    # Open configuration file
+    # Get telemetry databse name from configuration file
     dbFilename = telemetryConfig.get("database", "filename")
 
     # Connect to database, create SQL query, execute query, and close database
     try:
         conn = sqlite3.connect(dbFilename)
-        conn.row_factory = sqlite3.Row
+        conn.row_factory = sqlite3.Row  # SQLite.Row returns columns,values
+        cur = conn.cursor()
+        cur.execute(sql,paramTuple)
+        rows = cur.fetchall()
 
     except StandardError as e:
         logger.error("StandardError: " + str(e))
@@ -509,14 +577,9 @@ def queryStationsDb(parameters):
     except KeyError as e:
         logger.error("KeyError: " + str(e))
 
-    cur = conn.cursor()
-
-    sql = sqlBeg + sqlWhere + sqlEnd
-    print sql
+    # Parse through rows and create key:value dictionaries for each row.
+    # Then build up a list of dictionaries for all results.
     try:
-        cur.execute(sql,paramTuple)
-        rows = cur.fetchall()
-
         sqlData = []
         for row in rows:
             rowData = {}
@@ -533,12 +596,15 @@ def queryStationsDb(parameters):
     except KeyError as e:
         logger.error("KeyError: " + str(e))
 
-    # Completed query, close database
+    # Completed query, close database, return list of dictionary data for JSON
     conn.close()
-
     return sqlData
 
 def generateStartStopTimes(parameters):
+    """Use parameters dictionary to build up a Tuple of start/stop time values"""
+
+    # Check if start and stop times were provided in ISO 8610 format,
+    # if not then generate epoch from timespan
     if parameters["STARTTIME"] != None and parameters["ENDTIME"] != None:
         # Start end end times provided, ignore timespan
         startTime = str(parameters["STARTTIME"])
@@ -550,7 +616,6 @@ def generateStartStopTimes(parameters):
         endEpoch = time.time()
         startEpoch = endEpoch - float(parameters["TIMESPAN"])
         timeTuple = (int(startEpoch), int(endEpoch))
-        print timeTuple
 
     return timeTuple
 
