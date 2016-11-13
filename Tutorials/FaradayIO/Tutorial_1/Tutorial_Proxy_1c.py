889b782d39d880b329449554cec884756f0f365e
Brenton Salmi <brent@faradayrf.com>
2016-11-12 22:20:22 -0800

    Devide configuration works
    Works, is slow, not sure why so much waiting...

--- Tutorials/FaradayIO/Tutorial_1/Tutorial_Proxy_1c.py
+++ Tutorials/FaradayIO/Tutorial_1/Tutorial_Proxy_1c.py
@@ -31,26 +31,35 @@ faraday_parser = telemetryparser.TelemetryParse()
 
 #Display current device configuration prior to configuration flash update (Send UART telemetry update now command)
 #Send the command to read the entire Flash Memory Info D allocations
-faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, faraday_cmd.CommandLocalSendReadDeviceConfig())
-#Wait up to 1 second for the unit to respond to the command. NOTE: GETWait will return ALL packets received if more than 1 packet (likley not in THIS case)
-rx_flashd_data = faraday_1.GETWait(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, 1, False) #Will block and wait for given time until a packet is recevied
 
-#Decode the first packet in list from BASE 64 to a RAW bytesting
-rx_flashd_decoded = faraday_1.DecodeRawPacket(rx_flashd_data[0]['data'])
-rx_flashd_decoded_extracted = faraday_parser.ExtractPaddedPacket(rx_flashd_decoded, faraday_parser.flash_config_info_d_struct_len)
+try:
+    r = requests.get("http://127.0.0.1:8002", params={'callsign': "kb1lqd", 'nodeid':1})
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print e
 
-print "\n--- Current Device Flash Device Callsign Information ---\n"
-current_update_config = faraday_parser.UnpackConfigFlashD(rx_flashd_decoded_extracted, True)
+#Print JSON dictionary device data from unit
+print r.json()
 
 #########################################################################################
 ###Update configuration using INI file as defined by Faraday device object and functions
 #########################################################################################
 
+time.sleep(1) # Sleep to allow unit to process, polling and slow
+
 try:
-    requests.post('http://127.0.0.1:8002', params={'callsign':"kb1lqd", 'nodeid':1})
+    r = requests.post('http://127.0.0.1:8002', params={'callsign':"kb1lqd", 'nodeid':1})
 except requests.exceptions.RequestException as e:  # This is the correct syntax
     print e
 
+time.sleep(5) # Sleep to allow unit to process, polling and slow, not sure why THIS slow...
+
+try:
+    r = requests.get("http://127.0.0.1:8002", params={'callsign': "kb1lqd", 'nodeid':1})
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print e
+
+#Print JSON dictionary device data from unit
+print r.json()
 
 
 
