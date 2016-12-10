d90cb58eaba7ed74f27655fcd9af77aa1680f68b
Brenton Salmi <brent@faradayrf.com>
2016-12-10 13:19:35 -0800

    Documentation
    Added docstrings to msg_object.

--- Tutorials/Tutorials/2-Advanced_Proxy_Programs/Simple_Text_Messaging_Proxy_Application/msg_object.py
+++ Tutorials/Tutorials/2-Advanced_Proxy_Programs/Simple_Text_Messaging_Proxy_Application/msg_object.py
@@ -4,6 +4,12 @@ import Queue
 
 
 class TransmitObject(object):
+    """
+    This is the receiver object that provided receive function interface and buffering.
+
+    :param local_device_callsign: The callsign of the proxy assigned local Faraday device to connect to
+    :param local_device_node_id: The callsign ID number of the proxy assigned local Faraday device to connect to
+    """
     def __init__(self, local_device_callsign, local_device_node_id):
         self.local_device_callsign = local_device_callsign
         self.local_device_node_id = local_device_node_id
@@ -17,6 +23,15 @@ class TransmitObject(object):
         self.GETWAIT_TIMEOUT = 2
 
     def send(self, dest_callsign, dest_id, payload):
+        """
+        This function transmits a supplied function argument message (text/binary) to the intended remote Faraday
+        unit.
+
+        :param dest_callsign: The callsign of the remote Faraday unit to address the transmission to
+        :param dest_id: The callsign ID number of the remote Faraday unit to address the transmission to
+        :param payload: The data (text/binary) to send to the remote Faraday unit
+
+        """
         # Create message fragments
         self.faraday_tx_msg_sm.createmsgpackets(self.local_device_callsign, self.local_device_node_id, payload)
 
@@ -27,6 +42,12 @@ class TransmitObject(object):
 
 
 class ReceiveObject(threading.Thread):
+    """
+    This is the receiver object that provided receive function interface and buffering.
+
+    :param local_device_callsign: The callsign of the proxy assigned local Faraday device to connect to
+    :param local_device_node_id: The callsign ID number of the proxy assigned local Faraday device to connect to
+    """
     def __init__(self, local_device_callsign, local_device_node_id):
         self.local_device_callsign = local_device_callsign
         self.local_device_node_id = local_device_node_id
@@ -41,6 +62,10 @@ class ReceiveObject(threading.Thread):
         return
 
     def run(self):
+        """
+            This is the main threaded loop that the receiver program runs to obtain new received data packets from
+            the proxy interface. Received data packets are saved in to FIFO for later retrieval.
+            """
         # Loop continuously through the faraday experimental RF command message application RX routine
         while 1:
             rx_message_dict = self.faraday_rx_msg_object.rxmsgloop(self.local_device_callsign,
@@ -54,13 +79,30 @@ class ReceiveObject(threading.Thread):
                 pass  # No messages received
 
     def getqueuesize(self):
+        """
+        This function returns the estimated number of data packets that have been received and are able to be
+        retrieved from the receive FIFO.
+
+        :returns self.fifo.qsize()
+
+        """
         return self.fifo.qsize()
 
     def getqueueitem(self):
+        """
+        This function returns the next data item from the receiver FIFO. If no items are available the function will
+        return False.
+
+        :returns self.fifo.get_nowait()
+
+        """
         return self.fifo.get_nowait()
 
 
 class MessageObject(object):
+    """
+    This object is used to create a single transmit/receive object for a single connected proxy Faraday device.
+    """
     def __init__(self, local_device_callsign, local_device_node_id):
         self.transmit = transmit_object(local_device_callsign, local_device_node_id)
         self.receive = receive_object(local_device_callsign, local_device_node_id)
