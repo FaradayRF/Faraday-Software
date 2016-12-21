b8bcc42f75cf54c45ccbcfb066ca9aa405efc05a
Brenton Salmi <brent@faradayrf.com>
2016-12-21 11:29:35 -0800

    Updated factory reset tutorial

--- Tutorials/Tutorials/1-Basic_Proxy_Interactions_And_Programming/6-Factory-Reset-Configuration/Factory_Reset.py
+++ Tutorials/Tutorials/1-Basic_Proxy_Interactions_And_Programming/6-Factory-Reset-Configuration/Factory_Reset.py
@@ -9,6 +9,8 @@ from FaradayIO import faradaycommands
 from FaradayIO import telemetryparser
 from FaradayIO import cc430radioconfig
 
+import time
+
 
 #Variables
 local_device_callsign = 'KB1LQD' # Should match the connected Faraday unit as assigned in Proxy configuration
@@ -25,31 +27,43 @@ faraday_parser = telemetryparser.TelemetryParse()
 ## Telemetry
 ############
 
-#Flush old data from UART service port
-faraday_1.FlushRxPort(local_device_callsign, local_device_node_id, faraday_1.TELEMETRY_PORT)
+def get_telemetry():
+    #Flush old data from UART service port
+    faraday_1.FlushRxPort(local_device_callsign, local_device_node_id, faraday_1.TELEMETRY_PORT)
 
-#Command UART Telemetry Update NOW
-faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, faraday_cmd.CommandLocalUARTFaradayTelemetry())
+    #Command UART Telemetry Update NOW
+    faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, faraday_cmd.CommandLocalUARTFaradayTelemetry())
 
-#Wait up to 1 second for the unit to respond to the command. NOTE: GETWait will return ALL packets received if more than 1 packet (likley not in THIS case)
-rx_telem_data = faraday_1.GETWait(local_device_callsign, local_device_node_id,faraday_1.TELEMETRY_PORT, 1) #Will block and wait for given time until a packet is recevied
+    #Wait up to 1 second for the unit to respond to the command. NOTE: GETWait will return ALL packets received if more than 1 packet (likley not in THIS case)
+    rx_telem_data = faraday_1.GETWait(local_device_callsign, local_device_node_id,faraday_1.TELEMETRY_PORT, 1) #Will block and wait for given time until a packet is recevied
 
-#Decode the first packet in list from BASE 64 to a RAW bytesting
-rx_telem_pkt_decoded = faraday_1.DecodeRawPacket(rx_telem_data[0]['data'])
+    #Decode the first packet in list from BASE 64 to a RAW bytesting
+    rx_telem_pkt_decoded = faraday_1.DecodeRawPacket(rx_telem_data[0]['data'])
 
-#Unpack the telemetry datagram containing the standard "Telemetry Packet #3" packet
-rx_telemetry_datagram = faraday_parser.UnpackDatagram(rx_telem_pkt_decoded) #Debug is OFF
-rx_telemetry_packet = rx_telemetry_datagram['PayloadData']
+    #Unpack the telemetry datagram containing the standard "Telemetry Packet #3" packet
+    rx_telemetry_datagram = faraday_parser.UnpackDatagram(rx_telem_pkt_decoded) #Debug is OFF
+    rx_telemetry_packet = rx_telemetry_datagram['PayloadData']
 
-#Extract the exact debug packet from longer datagram payload (Telemetry Packet #3)
-rx_telemetry_datagram_extracted = faraday_parser.ExtractPaddedPacket(rx_telemetry_packet, faraday_parser.packet_3_len)
+    #Extract the exact debug packet from longer datagram payload (Telemetry Packet #3)
+    rx_telemetry_datagram_extracted = faraday_parser.ExtractPaddedPacket(rx_telemetry_packet, faraday_parser.packet_3_len)
 
-#Parse the Telemetry #3 packet
-rx_telemetry_packet_parsed = faraday_parser.UnpackPacket_3(rx_telemetry_datagram_extracted, debug = True) #Debug ON
+    #Parse the Telemetry #3 packet
+    rx_telemetry_packet_parsed = faraday_parser.UnpackPacket_3(rx_telemetry_datagram_extracted, debug = True) #Debug ON
 
-print "Parsed packet dictionary:", rx_telemetry_packet_parsed
+    print "Parsed packet dictionary:", rx_telemetry_packet_parsed
 
-## Reset Device
+def factory_reset_configuration():
+    #Command  Unit Factory Reset
+    faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, faraday_cmd.CommandLocalFactoryResetConfiguration())
 
-#Command  Unit Factory Reset
-faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, faraday_cmd.CommandLocalFactoryResetConfiguration())
+
+# Get pre-reset general telemetry
+get_telemetry()
+
+# Factory reset device
+factory_reset_configuration()
+
+time.sleep(3)
+
+# Get post-reset general telemetry
+get_telemetry()
