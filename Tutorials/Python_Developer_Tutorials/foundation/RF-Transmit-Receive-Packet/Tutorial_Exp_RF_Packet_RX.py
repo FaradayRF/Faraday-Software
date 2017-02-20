a92711e492f555b7a48044a529871492f7aade7a
Brenton Salmi <brent@faradayrf.com>
2017-02-20 00:59:09 -0800

    RX and TX fixed updated to INI and working.

--- Tutorials/Python_Developer_Tutorials/foundation/RF-Transmit-Receive-Packet/Tutorial_Exp_RF_Packet_RX.py
+++ Tutorials/Python_Developer_Tutorials/foundation/RF-Transmit-Receive-Packet/Tutorial_Exp_RF_Packet_RX.py
@@ -18,12 +18,12 @@ config.read(filename)
 #Definitions
 
 #Variables
-local_device_callsign = config.get("DEVICES","UNIT0CALL") # Should match the connected Faraday unit as assigned in Proxy configuration
-local_device_node_id = config.getint("DEVICES","UNIT0ID") # Should match the connected Faraday unit as assigned in Proxy configuration
-local_device_callsign = str(local_device_callsign).upper()
-remote_device_callsign = config.get("DEVICES","UNIT1CALL") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
-remote_device_node_id = config.getint("DEVICES","UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
-remote_device_callsign = str(remote_device_callsign).upper()
+transmitter_device_callsign = config.get("DEVICES", "UNIT0CALL") # Should match the connected Faraday unit as assigned in Proxy configuration
+transmitter_device_node_id = config.getint("DEVICES", "UNIT0ID") # Should match the connected Faraday unit as assigned in Proxy configuration
+transmitter_device_callsign = str(transmitter_device_callsign).upper()
+receiver_device_callsign = config.get("DEVICES", "UNIT1CALL") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
+receiver_device_node_id = config.getint("DEVICES", "UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
+receiver_device_callsign = str(receiver_device_callsign).upper()
 
 #Start the proxy server after configuring the configuration file correctly
 #Setup a Faraday IO object
@@ -36,19 +36,20 @@ PROXY_GET_TIMEOUT = 1 # Second(s)
 
 #Print debug information about proxy port listening
 print "Receiver operating TCP Localhost port:", faraday_1.FLASK_PORT
-
+print "Receiver (Proxy):", receiver_device_callsign + '-' + str(receiver_device_node_id)
+print "\n"
 #Setup variables for receiving
 data = None
 
 #While loop to wait for reception of data packet from experimental message application
 while(1):
     #Wait until there is new data on the message application port OR timout
-    data = faraday_1.GETWait(local_device_callsign, local_device_node_id, PROXY_MESSAGE_EXPERIMENTAL_PORT, 2)
+    data = faraday_1.GETWait(receiver_device_callsign, receiver_device_node_id, PROXY_MESSAGE_EXPERIMENTAL_PORT, 2)
 
     #Check if data is False (False means that the Get() function timed out), if not then display new data
     if (data != None) and (not 'error' in data):
-        #print "Received Message RAW", repr(data[0]['data'])
-        print "\nReceived Message Decoded:", faraday_1.DecodeRawPacket(data[0]['data'])
+        payload_data = data[0]['data']
+        print "Received Message Decoded:", faraday_1.DecodeRawPacket(payload_data)
 
         #Set data = False so that the function loop can properly wait until the next data without printing last received data over and over
         data = None
--- Tutorials/Python_Developer_Tutorials/foundation/RF-Transmit-Receive-Packet/Tutorial_Exp_RF_Packet_TX.py
+++ Tutorials/Python_Developer_Tutorials/foundation/RF-Transmit-Receive-Packet/Tutorial_Exp_RF_Packet_TX.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 import ConfigParser
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../Faraday_Proxy_Tools")) #Append path to common tutorial FaradayIO module
@@ -18,32 +19,37 @@ config.read(filename)
 #Definitions
 
 #Variables
-local_device_callsign = config.get("DEVICES","UNIT0CALL") # Should match the connected Faraday unit as assigned in Proxy configuration
-local_device_node_id = config.getint("DEVICES","UNIT0ID") # Should match the connected Faraday unit as assigned in Proxy configuration
-local_device_callsign = str(local_device_callsign).upper()
-remote_device_callsign = config.get("DEVICES","UNIT1CALL") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
-remote_device_node_id = config.getint("DEVICES","UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
-remote_device_callsign = str(remote_device_callsign).upper()
+transmitter_device_callsign = config.get("DEVICES", "UNIT0CALL") # Should match the connected Faraday unit as assigned in Proxy configuration
+transmitter_device_node_id = config.getint("DEVICES", "UNIT0ID") # Should match the connected Faraday unit as assigned in Proxy configuration
+transmitter_device_callsign = str(transmitter_device_callsign).upper()
+receiver_device_callsign = config.get("DEVICES", "UNIT1CALL") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
+receiver_device_node_id = config.getint("DEVICES", "UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
+receiver_device_callsign = str(receiver_device_callsign).upper()
 
 #Start the proxy server after configuring the configuration file correctly
 #Setup a Faraday IO object
 faraday_1 = faradaybasicproxyio.proxyio()
 faraday_cmd = faradaycommands.faraday_commands()
 
-#Remote device information
-remote_device_callsign = 'KB1LQD'
-remote_device_node_id = 2
-
-print "\nConnecting to proxy on PROXY device:", local_device_callsign + '-' + str(local_device_node_id)
-print "Transmitting to device:", remote_device_callsign + '-' + str(remote_device_node_id)
+print "\nConnecting to proxy on PROXY device:", transmitter_device_callsign + '-' + str(transmitter_device_node_id)
+print "\nTransmitter (Proxy):", transmitter_device_callsign + '-' + str(transmitter_device_node_id)
+print "Receiver (Device Configuration):", receiver_device_callsign + '-' + str(receiver_device_node_id)
+print '\n'
 
 #Use the predefined experimental message command (singled packet) function to send an RF message to a remote unit
 message = "Testing RF Packet 1"
-command = faraday_cmd.CommandLocalExperimentalRfPacketForward(remote_device_callsign, remote_device_node_id, message)
+command = faraday_cmd.CommandLocalExperimentalRfPacketForward(receiver_device_callsign, receiver_device_node_id, message)
 print "Transmitting message:", message
-faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, command)
+faraday_1.POST(transmitter_device_callsign, transmitter_device_node_id, faraday_1.CMD_UART_PORT, command)
 
 message = "Testing RF Packet 2"
-command = faraday_cmd.CommandLocalExperimentalRfPacketForward(remote_device_callsign, remote_device_node_id, message)
+command = faraday_cmd.CommandLocalExperimentalRfPacketForward(receiver_device_callsign, receiver_device_node_id, message)
 print "Transmitting message:", message
-faraday_1.POST(local_device_callsign, local_device_node_id, faraday_1.CMD_UART_PORT, command)
+faraday_1.POST(transmitter_device_callsign, transmitter_device_node_id, faraday_1.CMD_UART_PORT, command)
+
+print "************************************"
+print "\nQuit with ctrl+c"
+while(True):
+    #Loop until user presses ctrl+c so they can read response
+    time.sleep(1)
+    pass
