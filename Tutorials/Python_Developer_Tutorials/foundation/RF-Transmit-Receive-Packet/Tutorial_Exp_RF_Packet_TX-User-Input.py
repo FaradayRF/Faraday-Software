133c18e655d0b2b24be7b8861930b63be39e2f69
Brenton Salmi <brent@faradayrf.com>
2017-02-20 01:02:57 -0800

    Removed extra variables.

--- Tutorials/Python_Developer_Tutorials/foundation/RF-Transmit-Receive-Packet/Tutorial_Exp_RF_Packet_TX-User-Input.py
+++ Tutorials/Python_Developer_Tutorials/foundation/RF-Transmit-Receive-Packet/Tutorial_Exp_RF_Packet_TX-User-Input.py
@@ -25,10 +25,6 @@ receiver_device_callsign = config.get("DEVICES", "UNIT1CALL") # Should match the
 receiver_device_node_id = config.getint("DEVICES", "UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
 receiver_device_callsign = str(receiver_device_callsign).upper()
 
-#Variables
-transmitter_device_callsign = 'KB1LQD'  # Should match the connected Faraday unit as assigned in Proxy configuration
-transmitter_device_node_id = 1  # Should match the connected Faraday unit as assigned in Proxy configuration
-
 #Start the proxy server after configuring the configuration file correctly
 #Setup a Faraday IO object
 faraday_1 = faradaybasicproxyio.proxyio()
