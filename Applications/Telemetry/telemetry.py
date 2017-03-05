c26a906c62d08d484ef6804687d08dee798ae940
kb1lqc <bryce@faradayrf.com>
2017-03-04 22:47:44 -0800

    Removed commented out starttime code

--- Applications/Telemetry/telemetry.py
+++ Applications/Telemetry/telemetry.py
@@ -713,25 +713,6 @@ def queryStationsDb(parameters):
     sqlData = []
 
     timeTuple = generateStartStopTimes(parameters)
-    # # Check for whether a time range or timespan is being specified
-    # if parameters["STARTTIME"] != None and parameters["ENDTIME"] != None:
-    #     # Start end end times provided, ignore timespan
-    #     startTime = str(parameters["STARTTIME"])
-    #     endTime = str(parameters["ENDTIME"])
-    #     timeTuple = iso8601ToEpoch(startTime,endTime)
-    #     print startTime
-    #
-    # elif parameters["STARTTIME"] != None:
-    #     # Start time provided, use current time as end, ignore timespan
-    #     startTime = str(parameters["STARTTIME"])
-    #     endTime = time.strftime("%Y-%m-%dT%H:%M:%S")
-    #     timeTuple = iso8601ToEpoch(startTime,endTime)
-    #
-    # else:
-    #     # We should use the timespan provided to generate start and stop times
-    #     endEpoch = time.time()
-    #     startEpoch = endEpoch - int(parameters["TIMESPAN"])
-    #     timeTuple = (startEpoch, endEpoch)
 
     # Specify and create SQL command string
     sqlBeg = "SELECT SOURCECALLSIGN, SOURCEID, EPOCH FROM TELEMETRY "
