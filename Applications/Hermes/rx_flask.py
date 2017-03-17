c98a626fc067ff0034851f4a2d74995bdacfe74f
Brenton Salmi <brent@faradayrf.com>
2017-03-17 00:33:59 -0700

    Receiver user application now decoding full encoded JSON elemetn from message

--- Applications/Hermes/rx_flask.py
+++ Applications/Hermes/rx_flask.py
@@ -1,7 +1,11 @@
 import requests
+import base64
+import cPickle
 
-r = requests.get('http://127.0.0.1:8005/queue')
-print "Queue:", r.json()
+#r = requests.get('http://127.0.0.1:8005/queue')
+#print "Queue:", r.json()
 
 r = requests.get('http://127.0.0.1:8005')
-print "Message:", r.json()
+print "Message RAW:", r.json()
+print "Message Decoded Base64:", base64.b64decode(r.json())
+print "Message Decoded Base64 and Cpickle:", cPickle.loads(base64.b64decode(r.json()))
