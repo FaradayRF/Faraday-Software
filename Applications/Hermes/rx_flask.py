891e53522df37a57ba961d3854dde574ddcd3ddf
Brenton Salmi <brent@faradayrf.com>
2017-03-18 00:50:32 -0700

    Made a main function to run on start

--- Applications/Hermes/rx_flask.py
+++ Applications/Hermes/rx_flask.py
@@ -11,21 +11,23 @@ def getQueueSize():
         return queue_b64_pickle
 
 
-while 1:
-    #Sleep to release python process
-    time.sleep(0.5)
-    #Check if items in queue
-    queue_b64_pickle = getQueueSize()
-    if queue_b64_pickle['queuesize'] > 0:
-        rxdata = requests.get('http://127.0.0.1:8005')
-        rx_raw = rxdata.json()
-        rx_b64 = base64.b64decode(rxdata.json())
-        rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
-        print "\nFROM:", rx_b64_pickle['source_callsign']
-        print "Message:", rx_b64_pickle['message']
+def main():
+    while 1:
+        #Sleep to release python process
+        time.sleep(0.5)
+        #Check if items in queue
+        queue_b64_pickle = getQueueSize()
+        if queue_b64_pickle['queuesize'] > 0:
+            rxdata = requests.get('http://127.0.0.1:8005')
+            rx_raw = rxdata.json()
+            rx_b64 = base64.b64decode(rxdata.json())
+            rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
+            print "\nFROM:", rx_b64_pickle['source_callsign']
+            print "Message:", rx_b64_pickle['message']
 
 
-
+if __name__ == '__main__':
+    main()
 
 
 
