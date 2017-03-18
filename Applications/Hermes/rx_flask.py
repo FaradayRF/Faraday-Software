486437ce361da7e9c4bfdda04cc8b56a6cd0ee5f
Brenton Salmi <brent@faradayrf.com>
2017-03-18 00:36:21 -0700

    Modified user rx application to while 1 loop that checks for items in queue. Simplified output print as well.

--- Applications/Hermes/rx_flask.py
+++ Applications/Hermes/rx_flask.py
@@ -5,26 +5,22 @@ import cPickle
 #r = requests.get('http://127.0.0.1:8005/queue')
 #print "Queue:", r.json()
 
-queuelen = requests.get('http://127.0.0.1:8005/queue')
+while 1:
+    #Check if items in queue
+    queuelen = requests.get('http://127.0.0.1:8005/queue')
+    queue_raw = queuelen.json()
+    queue_b64 = base64.b64decode(queuelen.json())
+    queue_b64_pickle = cPickle.loads(base64.b64decode(queuelen.json()))
+    if queue_b64_pickle['queuesize'] > 0:
+        rxdata = requests.get('http://127.0.0.1:8005')
+        rx_raw = rxdata.json()
+        rx_b64 = base64.b64decode(rxdata.json())
+        rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
+        print "\nFROM:", rx_b64_pickle['source_callsign']
+        print "Message:", rx_b64_pickle['message']
 
 
-queue_raw = queuelen.json()
-queue_b64 = base64.b64decode(queuelen.json())
-queue_b64_pickle = cPickle.loads(base64.b64decode(queuelen.json()))
 
-print queue_raw
-print queue_b64_pickle
 
-rxdata = requests.get('http://127.0.0.1:8005')
 
-rx_raw = rxdata.json()
-rx_b64 = base64.b64decode(rxdata.json())
-rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
-
-print "\nFROM:", rx_b64_pickle['source_callsign']
-print "Message:", rx_b64_pickle['message']
-
-print '\n**** DEBUG INFO ****'
-print "Message RAW (Base64):", rx_raw
-print "Message Decoded Base64 and Cpickle:", rx_b64_pickle
 
