9d125c8d73ca915fdd535b8532befd97cfb77349
Brenton Salmi <brent@faradayrf.com>
2017-03-18 18:29:36 -0700

    Renamned objects to general faraday names to remove receiver only references. Looking to make object RX and TX.

--- Applications/Hermes/hermesflask.py
+++ Applications/Hermes/hermesflask.py
@@ -18,17 +18,17 @@ config.read(filename)
 
 #Definitions
 
-#Variables
-transmitter_device_callsign = config.get("DEVICES", "UNIT0CALL") # Should match the connected Faraday unit as assigned in Proxy configuration
-transmitter_device_node_id = config.getint("DEVICES", "UNIT0ID") # Should match the connected Faraday unit as assigned in Proxy configuration
-transmitter_device_callsign = str(transmitter_device_callsign).upper()
-receiver_device_callsign = config.get("DEVICES", "UNIT1CALL") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
-receiver_device_node_id = config.getint("DEVICES", "UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
-receiver_device_callsign = str(receiver_device_callsign).upper()
+# #Variables
+# transmitter_device_callsign = config.get("DEVICES", "UNIT0CALL") # Should match the connected Faraday unit as assigned in Proxy configuration
+# transmitter_device_node_id = config.getint("DEVICES", "UNIT0ID") # Should match the connected Faraday unit as assigned in Proxy configuration
+# transmitter_device_callsign = str(transmitter_device_callsign).upper()
+faradaycallsign = config.get("DEVICES", "UNIT1CALL") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
+faradaynodeid = config.getint("DEVICES", "UNIT1ID") # Should match the programmed callsign of the remote Faraday device to be commanded (receive)
+faradaycallsign = str(faradaycallsign).upper()
 
 
 # Create messaging unit objects with the two device connected to local proxy
-receiver = hermesobject.MessageObject(receiver_device_callsign, receiver_device_node_id)
+faradayhermesobj = hermesobject.MessageObject(faradaycallsign, faradaynodeid)
 
 
 # Initialize Flask microframework
@@ -46,7 +46,7 @@ def getMessage():
     #If GET
     else:
         #Get next message from queue
-        received_item = receiver.receive.getqueueitem()
+        received_item = faradayhermesobj.receive.getqueueitem()
 
         #Pickle dictionary data
         received_data_pickle = cPickle.dumps(received_item)
@@ -63,7 +63,7 @@ def getQueue():
     """This function returns the number of packets in the receiver queue."""
     #Check Queue size of Unit #2 and receive packet (if recieved due to non-ARQ protocol)
     data = {}
-    data['queuesize'] = receiver.receive.getqueuesize()
+    data['queuesize'] = faradayhermesobj.receive.getqueuesize()
 
 
     # Pickle dictionary data
