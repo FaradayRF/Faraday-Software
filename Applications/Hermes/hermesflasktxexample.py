d7a232260d474c8ee529cfc4549443b55baad220
Brenton Salmi <brent@faradayrf.com>
2017-03-22 01:49:03 -0700

    Updated the transmit example for flask to get user input

--- Applications/Hermes/hermesflasktxexample.py
+++ Applications/Hermes/hermesflasktxexample.py
@@ -6,9 +6,10 @@ import time
 
 
 def main():
-    message = "Hello World!"
-    payload = {'localcallsign': 'kb1lqd', 'localnodeid': 1, 'destinationcallsign': 'kb1lqd', 'destinationnodeid': 2, 'data': message}
-    txdata = requests.post('http://127.0.0.1:8005/', params=payload)
+    while 1:
+        message = raw_input("Enter Message:")
+        payload = {'localcallsign': 'kb1lqd', 'localnodeid': 1, 'destinationcallsign': 'kb1lqd', 'destinationnodeid': 2, 'data': message}
+        txdata = requests.post('http://127.0.0.1:8005/', params=payload)
 
 if __name__ == '__main__':
     main()
