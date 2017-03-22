34bb6eeecb147e4d55e962958714509498c3a350
Brenton Salmi <brent@faradayrf.com>
2017-03-22 00:33:01 -0700

    Got RX working in kludgy but seeming correct FLASK fashion

--- Applications/Hermes/hermesflaskrxexample.py
+++ Applications/Hermes/hermesflaskrxexample.py
@@ -19,14 +19,15 @@ def main():
 
         print getQueueSize('kb1lqd', 2)
 
-        # #Check if items in queue
-        # while getQueueSize() > 0:
-        #     rxdata = requests.get('http://127.0.0.1:8005')
-        #     rx_raw = rxdata.json()
-        #     rx_b64 = base64.b64decode(rxdata.json())
-        #     rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
-        #     print "\nFROM:", rx_b64_pickle['source_callsign']
-        #     print "Message:", rx_b64_pickle['message']
+        #Check if items in queue
+        while getQueueSize('kb1lqd', 2) > 0:
+            payload = {'localcallsign': 'kb1lqd', 'localnodeid': 2}
+            rxdata = requests.get('http://127.0.0.1:8005/', params=payload)
+            rx_raw = rxdata.json()
+            rx_b64 = base64.b64decode(rxdata.json())
+            rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
+            print "\nFROM:", rx_b64_pickle['source_callsign']
+            print "Message:", rx_b64_pickle['message']
 
 
 if __name__ == '__main__':
