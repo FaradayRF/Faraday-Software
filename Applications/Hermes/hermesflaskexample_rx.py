1cd0d4ea566c47257037cefd435932066e23ad3e
Brenton Salmi <brent@faradayrf.com>
2017-03-23 00:57:32 -0700

    Added docstrings to RX example

--- Applications/Hermes/hermesflaskexample_rx.py
+++ Applications/Hermes/hermesflaskexample_rx.py
@@ -15,6 +15,12 @@ localnodeid = int(config.get('UNIT0', 'NODEID'))
 
 
 def getQueueSize(callsign, nodeid):
+    """
+    :param callsign: Callsign of the local device being queried
+    :param nodeid: Node ID of the local device being queried
+    :return: The RX queue size
+    """
+
         payload = {'localcallsign': callsign, 'localnodeid': int(nodeid)}
         queuelen = requests.get('http://127.0.0.1:8005/queue', params = payload)
         queue_raw = queuelen.json()
@@ -24,6 +30,12 @@ def getQueueSize(callsign, nodeid):
 
 
 def main():
+    """
+    Main function of the receive example of Hermes messaging application using Flask. This function loops continuously
+    to check the Hermes Flask server receiver for the intended local Faraday device for new data to retrieve. It checks
+    if data is present by querying the receive queue size, if data is present it retrieves ALL data each packet at a
+    time until the queue is empty.
+    """
     while 1:
         #Sleep to release python process
         time.sleep(0.1)
