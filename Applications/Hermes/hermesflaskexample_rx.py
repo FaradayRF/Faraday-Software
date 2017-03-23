60efb404e3b7d6cf2ce7c988a13cc805d3cfbcfa
Brenton Salmi <brent@faradayrf.com>
2017-03-23 01:46:29 -0700

    PEP8 pass on RX example

--- Applications/Hermes/hermesflaskexample_rx.py
+++ Applications/Hermes/hermesflaskexample_rx.py
@@ -5,7 +5,7 @@ import time
 import ConfigParser
 import os
 
-#Open configuration INI
+# Open configuration INI
 config = ConfigParser.RawConfigParser()
 filename = os.path.abspath("hermes.ini")
 config.read(filename)
@@ -14,7 +14,7 @@ localcallsign = config.get('UNIT0', 'CALLSIGN')
 localnodeid = int(config.get('UNIT0', 'NODEID'))
 
 
-def getRxQueueSize(callsign, nodeid):
+def getrxqueuesize(callsign, nodeid):
     """
     :param callsign: Callsign of the local device being queried
     :param nodeid: Node ID of the local device being queried
@@ -22,9 +22,7 @@ def getRxQueueSize(callsign, nodeid):
     """
 
     payload = {'localcallsign': callsign, 'localnodeid': int(nodeid)}
-    queuelen = requests.get('http://127.0.0.1:8005/queue', params = payload)
-    queue_raw = queuelen.json()
-    queue_b64 = base64.b64decode(queuelen.json())
+    queuelen = requests.get('http://127.0.0.1:8005/queue', params=payload)
     queue_b64_pickle = cPickle.loads(base64.b64decode(queuelen.json()))
     return queue_b64_pickle['queuesize']
 
@@ -37,15 +35,13 @@ def main():
     time until the queue is empty.
     """
     while 1:
-        #Sleep to release python process
+        # Sleep to release python process
         time.sleep(0.1)
 
-        #Check if items in queue
-        while getRxQueueSize(localcallsign, localnodeid) > 0:
+        # Check if items in queue
+        while getrxqueuesize(localcallsign, localnodeid) > 0:
             payload = {'localcallsign': localcallsign, 'localnodeid': localnodeid}
             rxdata = requests.get('http://127.0.0.1:8005/', params=payload)
-            rx_raw = rxdata.json()
-            rx_b64 = base64.b64decode(rxdata.json())
             rx_b64_pickle = cPickle.loads(base64.b64decode(rxdata.json()))
             print "\nFROM:", rx_b64_pickle['source_callsign']
             print "Message:", rx_b64_pickle['message']
@@ -53,6 +49,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
