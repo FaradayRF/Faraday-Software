3a8bc0d3d7afb4a5d61d6717140dd62db1bef902
Brenton Salmi <brent@faradayrf.com>
2017-03-23 01:48:07 -0700

    PEP8 pass on TX example

--- Applications/Hermes/hermesflaskexample_tx.py
+++ Applications/Hermes/hermesflaskexample_tx.py
@@ -1,7 +1,4 @@
 import requests
-import base64
-import cPickle
-import time
 import ConfigParser
 import os
 
@@ -14,18 +11,18 @@ localnodeid = int(config.get('UNIT1', 'NODEID'))
 destinationcallsign = config.get('UNIT0', 'CALLSIGN')
 destinationnodeid = int(config.get('UNIT0', 'NODEID'))
 
+
 def main():
     """
-    Main function of the transmit example of Hermes messaging application using Flask. This function loops continously
+    Main function of the transmit example of Hermes messaging application using Flask. This function loops continuously
     getting user input text to transmit to the Flask server for wireless transmission to the intended remote device.
     """
     while 1:
         message = raw_input("Enter Message: ")
-        payload = {'localcallsign': localcallsign, 'localnodeid': localnodeid, 'destinationcallsign': destinationcallsign, 'destinationnodeid': destinationnodeid, 'data': message}
-        txdata = requests.post('http://127.0.0.1:8005/', params=payload)
+        payload = {'localcallsign': localcallsign, 'localnodeid': localnodeid,
+                   'destinationcallsign': destinationcallsign, 'destinationnodeid': destinationnodeid, 'data': message}
+        requests.post('http://127.0.0.1:8005/', params=payload)
+
 
 if __name__ == '__main__':
     main()
-
-
-
