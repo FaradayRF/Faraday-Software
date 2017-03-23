aaffa4b5352e4ad5598112ebc79b5ebbc255d6e2
Brenton Salmi <brent@faradayrf.com>
2017-03-23 00:37:53 -0700

    Added config parsing to TX. Also slightly edited the message prmpt text.

--- Applications/Hermes/hermesflaskexample_tx.py
+++ Applications/Hermes/hermesflaskexample_tx.py
@@ -3,12 +3,19 @@ import base64
 import cPickle
 import time
 
+config = ConfigParser.RawConfigParser()
+filename = os.path.abspath("hermes.ini")
+config.read(filename)
 
+localcallsign = config.get('UNIT0', 'CALLSIGN')
+localnodeid = int(config.get('UNIT0', 'NODEID'))
+destinationcallsign = config.get('UNIT1', 'CALLSIGN')
+destinationnodeid = int(config.get('UNIT1', 'NODEID'))
 
 def main():
     while 1:
-        message = raw_input("Enter Message:")
-        payload = {'localcallsign': 'kb1lqd', 'localnodeid': 1, 'destinationcallsign': 'kb1lqd', 'destinationnodeid': 2, 'data': message}
+        message = raw_input("Enter Message: ")
+        payload = {'localcallsign': localcallsign, 'localnodeid': localnodeid, 'destinationcallsign': destinationcallsign, 'destinationnodeid': destinationnodeid, 'data': message}
         txdata = requests.post('http://127.0.0.1:8005/', params=payload)
 
 if __name__ == '__main__':
