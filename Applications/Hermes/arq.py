6f9bd2094c0c69cf7d8d6624498d923ffa06337a
Brenton Salmi <brent@faradayrf.com>
2017-03-23 23:46:44 -0700

    Added initial state definitions

--- Applications/Hermes/arq.py
+++ Applications/Hermes/arq.py
@@ -4,7 +4,14 @@ import Queue
 
 
 # Definitions
-STATE_IDLE = 0
+STATE_IDLE = 0 # IDLE state to wait for commanded start state
+STATE_START = 1 # Reset counters/variables/etc... to default
+STATE_GETNEXTDATA = 2 # Get the next data item to be transmitted
+STATE_TX = 3 # Transmit data
+STATE_GETACK = 4 # Wait for acknowledgement from receiver
+STATE_RETRY = 5 # Retry current transmission data, update/check counters in case of timeout
+
+
 
 class transmitArqStateMachine(object):
 
@@ -23,4 +30,3 @@ class transmitArqStateMachine(object):
         """
         self.state = state
 
-    
