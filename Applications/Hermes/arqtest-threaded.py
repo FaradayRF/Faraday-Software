f6c4b05b6cea02bae835cd715a3f10a03faf7e62
Brenton Salmi <brent@faradayrf.com>
2017-03-25 22:29:56 -0700

    Adding threaded version of arq-test

---
+++ Applications/Hermes/arqtest-threaded.py
@@ -0,0 +1,58 @@
+import arq
+
+listdata = ['this ', 'is', ' a', ' test', '.']
+
+def transmitroutine( data):
+    print "Transmitting: ", data
+
+# Create object
+testtxsm = arq.TransmitArqStateMachine(listdata, transmitroutine)
+
+# Insert new data
+testtxsm.newdataqueue(listdata)
+
+# # Set state machine to start
+# testtxsm.updatestate(arq.STATE_START)
+#
+# # Run state machine operations - START
+# testtxsm.runstate()
+# print "1"
+#
+# # Run state machine operations - next Data
+# testtxsm.runstate()
+# print "2"
+#
+# # Run state machine operations - TX
+# testtxsm.runstate()
+# print '3'
+#
+# # Run state machine operations - Get ACK
+# testtxsm.runstate()
+# print '4'
+# # Receive ACK
+# testtxsm.ackreceived()
+# print '5'
+# testtxsm.runstate() # ACK RX'd now moving to next data
+#
+# print '6'
+#
+# # Get next data
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.ackreceived()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.ackreceived()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.ackreceived()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.ackreceived()
+# testtxsm.runstate()
+# testtxsm.runstate()
+# testtxsm.runstate()
