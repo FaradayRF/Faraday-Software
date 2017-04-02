45918dffb3c152a72205072151e8fc930fd0f5f8
Brenton Salmi <brent@faradayrf.com>
2017-04-01 22:12:52 -0700

    Basic PEP8 update for formatting

--- Applications/deviceconfiguration/readconfig.py
+++ Applications/deviceconfiguration/readconfig.py
@@ -1,10 +1,10 @@
-#Warning - Must run the "deviceconfiguration" proxy application
+# Warning - Must run the "deviceconfiguration" proxy application
 
 # readconfig.py is a simple script that only reads/displays the current programmed flash configuration without
 # programming the flash configuration of the unit as defined in the "deviceconfiguration.ini" configuration file.
 # Simply run the script with a valid "deviceconfiguration.ini" file
 
-#Imports - General
+# Imports - General
 
 import os
 import sys
@@ -14,28 +14,29 @@ import cPickle
 import time
 import ConfigParser
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../Faraday_Proxy_Tools")) #Append path to common tutorial FaradayIO module
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             "../../Faraday_Proxy_Tools"))  # Append path to common tutorial FaradayIO module
 
-#Imports - Faraday Specific
+# Imports - Faraday Specific
 from FaradayIO import faradaybasicproxyio
 from FaradayIO import faradaycommands
 from FaradayIO import telemetryparser
 
-#Open configuration INI
+# Open configuration INI
 config = ConfigParser.RawConfigParser()
 filename = os.path.abspath("deviceconfiguration.ini")
 config.read(filename)
 
-#Variables
-local_device_callsign = config.get("DEVICES","UNIT0CALL")
-local_device_node_id = config.get("DEVICES","UNIT0ID")
+# Variables
+local_device_callsign = config.get("DEVICES", "UNIT0CALL")
+local_device_node_id = config.get("DEVICES", "UNIT0ID")
 local_device_callsign = str(local_device_callsign).upper()
 
-hostname = config.get("FLASK","HOST")
-port = config.get("FLASK","PORT")
+hostname = config.get("FLASK", "HOST")
+port = config.get("FLASK", "PORT")
 
-#Start the proxy server after configuring the configuration file correctly
-#Setup a Faraday IO object
+# Start the proxy server after configuring the configuration file correctly
+# Setup a Faraday IO object
 faraday_1 = faradaybasicproxyio.proxyio()
 faraday_cmd = faradaycommands.faraday_commands()
 faraday_parser = telemetryparser.TelemetryParse()
@@ -46,7 +47,8 @@ faraday_parser = telemetryparser.TelemetryParse()
 
 
 try:
-    r = requests.get("http://127.0.0.1:8002", params={'callsign': str(local_device_callsign), 'nodeid': int(local_device_node_id)})
+    r = requests.get("http://127.0.0.1:8002",
+                     params={'callsign': str(local_device_callsign), 'nodeid': int(local_device_node_id)})
 except requests.exceptions.RequestException as e:
     # Some error occurred
     print r.text
@@ -58,7 +60,7 @@ raw_unit_json = r.json()
 b64_unit_json = base64.b64decode(raw_unit_json['data'])
 unit_configuration_dict = cPickle.loads(b64_unit_json)
 
-#Print configuration values
+# Print configuration values
 print "\n************************************"
 
 print "POST CONFIGURATION UPDATE"
@@ -85,16 +87,14 @@ print "DEFAULT_ALTITUDE:", str(unit_configuration_dict['default_altitude'])
 print "DEFAULT_ALTITUDE_UNITS:", str(unit_configuration_dict['default_altitude_units'])
 print "GPS_BOOT_BIT & GPS_PRESENT_BIT:", format(unit_configuration_dict['gps_boot_bitmask'], '#010b')
 print "\nTELEMETRY"
-print "UART_TELEMETRY_BOOT_BIT & RF_TELEMETRY_BOOT_BIT:", format(unit_configuration_dict['telemetry_boot_bitmask'], '#010b')
+print "UART_TELEMETRY_BOOT_BIT & RF_TELEMETRY_BOOT_BIT:", format(unit_configuration_dict['telemetry_boot_bitmask'],
+                                                                 '#010b')
 print "TELEMETRY_DEFAULT_UART_INTERVAL:", str(unit_configuration_dict['default_telemetry_uart_beacon_interval'])
 print "TELEMETRY_DEFAULT_RF_INTERVAL:", str(unit_configuration_dict['default_telemetry_rf_beacon_interval'])
 
 print "************************************"
 print "\nQuit with ctrl+c"
-while(True):
-    #Loop until user presses ctrl+c so they can read response
+while True:
+    # Loop until user presses ctrl+c so they can read response
     time.sleep(1)
     pass
-    
-
-
