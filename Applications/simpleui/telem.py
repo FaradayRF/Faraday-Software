215110c10f0789a15846f7f40e37efd2bc1f6731
Brenton Salmi <brent@faradayrf.com>
2017-04-06 21:04:38 -0700

    First running query for stations on telem server and config file

--- Applications/simpleui/telem.py
+++ Applications/simpleui/telem.py
@@ -1,1 +1,30 @@
-# Simple telemetry view
+import requests
+import base64
+import json
+import time
+import ConfigParser
+import os
+
+# Open configuration INI
+config = ConfigParser.RawConfigParser()
+filename = os.path.abspath("simpleui.ini")
+config.read(filename)
+
+telemhost = config.get('FLASKTELEM', 'HOST')
+telemport = int(config.get('FLASKTELEM', 'PORT'))
+
+localcallsign = config.get('UNITS', 'UNIT0CALL')
+localnodeid = int(config.get('UNITS', 'UNIT0ID'))
+
+
+def getstations():
+    """
+    Get stations heard.
+    """
+
+    queuelen = requests.get('http://' + str(telemhost) + ':' + str(telemport) + '/stations')
+    queuesize = queuelen.json()
+
+    return queuesize
+
+print getstations()
