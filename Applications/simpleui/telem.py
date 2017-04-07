12e6345fc1f55595f929e8153d503188d8afd594
Brenton Salmi <brent@faradayrf.com>
2017-04-07 00:30:31 -0700

    Added basic printing of telemetry data

--- Applications/simpleui/telem.py
+++ Applications/simpleui/telem.py
@@ -77,8 +77,74 @@ def getStationData(stations, age):
         else:
             stationData.append(data)
 
+    # Return on single dictionary
+    stationData = stationData[0][0]
     # Return all detailed stationData
     return stationData
 
-units = getStations()
-print getStationData(units, 60)
+def displayData(telempacket):
+
+    #print len(telempacket)
+    #print telempacket
+    print "Callsign ----- ", telempacket['SOURCECALLSIGN'] + '-' + str(telempacket['SOURCEID'])
+    print "GPS Altitude ----- ", telempacket['GPSALTITUDE']
+    # print "Source Callsign Length", dictionaryData['SOURCECALLSIGNLEN']
+    # print "Source Callsign ID", dictionaryData['SOURCEID']
+    # print "Destination Callsign", dictionaryData['DESTINATIONCALLSIGN']
+    # print "Destination Callsign Length", dictionaryData['DESTINATIONCALLSIGNLEN']
+    # print "Destination Callsign ID", dictionaryData['DESTINATIONID']
+    # print "RTC Second", dictionaryData['RTCSEC']
+    # print "RTC Minute", dictionaryData['RTCMIN']
+    # print "RTC Hour", dictionaryData['RTCHOUR']
+    # print "RTC Day", dictionaryData['RTCDAY']
+    # print "RTC Day Of Week", dictionaryData['RTCDOW']
+    # print "RTC Month", dictionaryData['RTCMONTH']
+    # print "Year", dictionaryData['RTCYEAR']
+    # print "GPS Lattitude", dictionaryData['GPSLATITUDE']
+    # print "GPS Lattitude Direction", dictionaryData['GPSLATITUDEDIR']
+    # print "GPS Longitude", dictionaryData['GPSLONGITUDE']
+    # print "GPS Longitude Direction", dictionaryData['GPSLONGITUDEDIR']
+    # print "GPS Altitude", dictionaryData['GPSALTITUDE']
+    # print "GPS Altitude Units", dictionaryData['GPSALTITUDEUNITS']
+    # print "GPS Speed", dictionaryData['GPSSPEED']
+    # print "GPS Fix", dictionaryData['GPSFIX']
+    # print "GPS HDOP", dictionaryData['GPSHDOP']
+    # print "GPIO State Telemetry", dictionaryData['GPIOSTATE']
+    # print "IO State Telemetry", dictionaryData['IOSTATE']
+    # print "RF State Telemetry", dictionaryData['RFSTATE']
+    # print "ADC 0", dictionaryData['ADC0']
+    # print "ADC 1", dictionaryData['ADC1']
+    # print "ADC 2", dictionaryData['ADC2']
+    # print "ADC 3", dictionaryData['ADC3']
+    # print "ADC 4", dictionaryData['ADC4']
+    # print "ADC 5", dictionaryData['ADC5']
+    # print "VCC", dictionaryData['VCC']
+    # print "CC430 Temperature", dictionaryData['BOARDTEMP']
+    # print "ADC 8", dictionaryData['ADC8']
+    # # print "N/A Byte", dictionaryData['']
+    # print "HAB Automatic Cutdown Timer State Machine State", dictionaryData['HABTIMERSTATE']
+    # print "HAB Cutdown Event State Machine State", dictionaryData['HABCUTDOWNSTATE']
+    # print "HAB Automatic Cutdown Timer Trigger Time", dictionaryData['HABTRIGGERTIME']
+    # print "HAB Automatic Cutdown Timer Current Time", dictionaryData['HABTIMER']
+    # print "EPOCH", dictionaryData['EPOCH']
+
+
+def main():
+    """
+    Main function which starts telemery worker thread + Flask server.
+
+    :return: None
+    """
+
+    logger.info('Starting Faraday SimpleUI application')
+
+    while True:
+        telempacket = getStationData(getStations(), 60)
+        displayData(telempacket)
+        time.sleep(1)
+
+
+
+
+if __name__ == '__main__':
+    main()
