2b039b257a99ce723321ce144ed8e28fa0dddccf
Bryce Salmi <bryce@faradayrf.com>
2017-04-11 01:24:17 -0700

    Some basic ability to command LEDs
    Only manual on/off but something. Stopping for the night. Need to figure
    out a path forward soon.

--- Applications/SimpleUI/simpleui.py
+++ Applications/SimpleUI/simpleui.py
@@ -10,12 +10,22 @@ import json
 import logging.config
 import ConfigParser
 import os
+import sys
 
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
 from flask_bootstrap import Bootstrap
+
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             "../../Faraday_Proxy_Tools"))  # Append path to common tutorial FaradayIO module
+
+# Imports - Faraday Specific
+from FaradayIO import faradaybasicproxyio
+from FaradayIO import faradaycommands
+from FaradayIO import gpioallocations
+
 
 
 # Start logging after importing modules
@@ -50,9 +60,29 @@ def simpleui():
         else:
             return "Please provide a callsign and nodeid in URL i.e. <br />localhost/?callsign=nocall&nodeid=1"
     if request.method == "POST":
+        # Start the proxy server after configuring the configuration file correctly
+        # Setup a Faraday IO object
+        faraday_1 = faradaybasicproxyio.proxyio()  # default proxy port
+        faraday_cmd = faradaycommands.faraday_commands()
+
+        command = faraday_cmd.CommandLocalGPIOLED1Off()
+        faraday_1.POST("KB1LQC", 1, faraday_1.CMD_UART_PORT, command)
+
         logger.info(request.form)
         callsign = request.form["CALLSIGN"]
         nodeid = request.form["NODEID"]
+
+        # # Could replace with dict?
+        # if request.form["LED1ON"] == "on":
+        #     command = faraday_cmd.CommandLocalGPIOLED1On()
+        # if request.form["LED1OFF"] == "on":
+        #     command = faraday_cmd.CommandLocalGPIOLED1Off()
+        # try:
+        #     faraday_1.POST("KB1LQC", 1, faraday_1.CMD_UART_PORT, command)
+        # except StandardError as e:
+        #     print e
+
+
         return redirect("http://localhost/?callsign={0}&nodeid={1}".format(callsign,nodeid), code=302)
 
 @app.errorhandler(404)
