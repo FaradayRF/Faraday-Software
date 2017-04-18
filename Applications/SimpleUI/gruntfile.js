b6772960bb18d3627d906d2decbfef736021da66
Bryce Salmi <bryce@faradayrf.com>
2017-04-18 00:46:55 -0700

    Updated Gruntfile to perform jshint and html error
    HTML title added to both index.html and layout.html to mitigate an error
    in htmllint. I should actually ignore it in index.html but moving on...
    Added support for jshint.

--- Applications/SimpleUI/gruntfile.js
+++ Applications/SimpleUI/gruntfile.js
@@ -3,13 +3,26 @@ module.exports = function(grunt) {
   grunt.initConfig({
     htmllint: {
       all: ["templates/*.html"]
+    },
+    jshint: {
+        // define the files to lint
+    files: ['Gruntfile.js', 'static/simpleui.js'],
+        // configure JSHint (documented at http://www.jshint.com/docs/)
+    options: {
+        // more options here if you want to override JSHint defaults
+        globals: {
+            jQuery: true,
+            console: true,
+            module: true
+            }
+        }
     }
     });
 
   grunt.loadNpmTasks('grunt-html');
-  //grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   //grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['htmllint']);
+  grunt.registerTask('default', ['htmllint','jshint']);
 
 };
