59abaee201383bbd49f5546771317582aab212e8
Bryce Salmi <bryce@faradayrf.com>
2017-04-27 00:53:55 -0700

    Added initial aprs.py Application file
    Imported recently moved faraday/aprs.py as a module successfully! Runs
    fine.

---
+++ Applications/APRS/aprs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+# Add Faraday library to the Python path.
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
+from faraday import aprs
+
+aprs.main()
