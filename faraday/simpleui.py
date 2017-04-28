cc8a5521523033a2f419cc672d8f554a59aceec5
Bryce Salmi <bryce@faradayrf.com>
2017-04-28 00:05:10 -0700

    Moved template folder back to Applications/
    Moved the templates folder back to Applications/SimpleUI/templates and
    updated the "template_folder=x" parameter in the Flask app.

--- faraday/simpleui.py
+++ faraday/simpleui.py
@@ -38,7 +38,7 @@ filename = os.path.abspath("simpleui.ini")
 simpleuiconfig.read(filename)
 
 # Initialize Flask microframework
-app = Flask(__name__, static_folder='../Applications/SimpleUI/static')
+app = Flask(__name__, static_folder='../Applications/SimpleUI/static', template_folder='../Applications/SimpleUI/templates')
 
 
 @app.route('/', methods=['GET', 'POST'])
