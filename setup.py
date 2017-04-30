dc61bf13d461f14ffd421f65a086b4c17ff2c1e3
Bryce Salmi <bryce@faradayrf.com>
2017-04-30 15:02:41 -0700

    Added setup.py
    Initial add of setup.py for setuptools use when installing with pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# Name:        /setup.py
+# Purpose:     Python setuptools setup.py file contains all information necessary
+#              to install faraday software with pip
+#
+# Author:      Bryce Salmi
+#
+# Created:     4/30/2017
+# Licence:     GPLv3
+#-------------------------------------------------------------------------------
