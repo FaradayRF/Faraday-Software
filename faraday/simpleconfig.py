6f11280a2a223f696a2573108bf103c9a2c9a6ba
kb1lqc <bryce@faradayrf.com>
2017-06-29 06:53:25 -0700

    Updated simpleconfig for correct imports
    Added path importing functions to import logging and deviceconfig INI
    files correctly.

--- faraday/simpleconfig.py
+++ faraday/simpleconfig.py
@@ -11,18 +11,34 @@ import base64
 import time
 import ConfigParser
 import json
+import logging.config
 
-# Add Faraday library to the Python path.
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
 #Imports - Faraday Specific
 from faraday.proxyio import faradaybasicproxyio
 from faraday.proxyio import faradaycommands
 from faraday.proxyio import telemetryparser
 
+# Start logging after importing modules
+relpath1 = os.path.join('etc', 'faraday')
+relpath2 = os.path.join('..', 'etc', 'faraday')
+setuppath = os.path.join(sys.prefix, 'etc', 'faraday')
+userpath = os.path.join(os.path.expanduser('~'), '.faraday')
+path = ''
+
+for location in os.curdir, relpath1, relpath2, setuppath, userpath:
+    try:
+        logging.config.fileConfig(os.path.join(location, "loggingConfig.ini"))
+        path = location
+        break
+    except ConfigParser.NoSectionError:
+        pass
+
+logger = logging.getLogger('SimpleConfig')
+
 #Open configuration INI
 config = ConfigParser.RawConfigParser()
-filename = os.path.abspath("deviceconfiguration.ini")
+filename = os.path.abspath(os.path.join(path, "deviceconfiguration.ini"))
 config.read(filename)
 
 #Variables
