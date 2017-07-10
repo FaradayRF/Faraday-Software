d291144851b7d5c473acc17a1d968cbe3882c4d0
Brenton Salmi <brent@faradayrf.com>
2017-07-09 22:25:38 -0700

    Updating programming script

--- simple_scripts/program_unit.py
+++ simple_scripts/program_unit.py
@@ -3,15 +3,26 @@ import os
 import time
 
 # Proxy
-proxy_callsign = 'kb1lqd'
-proxy_nodeid = 2
-proxy_port = 'COM112'
-proxy_unitcnt = 1
+proxy_callsign = 'REPLACEME'
+proxy_nodeid = REPLACEME
+proxy_port = 'REPLACEME'
+
 
 # Device Configuration
-rfbootpower = 0
-uart_interval = 1
-rf_interval = 1
+proxy_unitcnt = 1  # Only program a single unit at a time!
+rfbootpower = 20  # MAX = 152
+uart_interval = 1  # Seconds
+rf_interval = 1  # Seconds
+# DEFAULT_LATITUDE MAX LENGTH = 9 Bytes in format "ddmm.mmmm" (including decimal)
+default_latitude='0000.0000'
+# DEFAULT_LATITUDE_DIRECTION MAX LENGTH = 1 Byte
+default_latitude_direction='N'
+# DEFAULT_LONGITUDE MAX LENGTH = 10 Bytes in format "dddmm.mmmm" (including decimal)
+default_longitude='00000.0000'
+# DEFAULT_LONGITUDE_DIRECTION MAX LENGTH = 1 Byte
+default_longitude_direction='W'
+# DEFAULT_ALTITUDE MAX LENGTH = 8 Bytes (including decimal) in meters
+default_altitude='00000.00'
 
 # initialize configurations files
 print ("--- INITIALIZING PROXY SERVER ---")
@@ -22,18 +33,26 @@ call(['faraday-proxy', '--unit', str(0), '--callsign', proxy_callsign, '--nodeid
 # initialize configurations files
 print ("--- INITIALIZING DEVICE CONFIGURATION SERVER ---")
 call(['faraday-deviceconfiguration', '--init-config'])
-call(['faraday-deviceconfiguration', '--init-faraday-config'])
+call(['faraday-deviceconfiguration', '--init-faraday-config'])  # Initialize faraday configuration INI holding program settings
+
+# Basics
 call(['faraday-deviceconfiguration', '--proxycallsign', proxy_callsign, '--proxynodeid', str(proxy_nodeid), '--nodeid', str(proxy_nodeid), '--callsign', proxy_callsign])
 call(['faraday-deviceconfiguration', '--redledtxon', '--greenledrxon'])
-call(['faraday-deviceconfiguration', '--gpsbooton', '--gpsenabled'])
+
+# GPS (NMEA DMM Format)
+#call(['faraday-deviceconfiguration', '--gpsbooton', '--gpsenabled'])
+call(['faraday-deviceconfiguration', '--gpsbootoff', '--gpsdisabled'])
+call(['faraday-deviceconfiguration', '--latitude', default_latitude, '--latitudedir', default_latitude_direction])
+call(['faraday-deviceconfiguration', '--longitude', default_longitude, '--longitudedir', default_longitude_direction])
+call(['faraday-deviceconfiguration', '--altitude', default_altitude])
+
+
+# Telemetry
 call(['faraday-deviceconfiguration', '--uarttelemetryenabled', '--uartinterval', str(uart_interval)])
 call(['faraday-deviceconfiguration', '--rftelemetryenabled', '--rfinterval', str(rf_interval)])
+
+# RF
 call(['faraday-deviceconfiguration', '--bootrfpower', str(rfbootpower)])
-
-# initialize configurations files
-#print ("--- INITIALIZING SIMPLECONFIG SERVER ---")
-#call(['faraday-simpleconfig', '--init-config'])
-
 
 # Start servers
 print ("--- STARTING PROXY SERVER ---")
