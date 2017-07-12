e361c7349a4b05a470c99dfa7cf79e9dab292d2c
kb1lqc <bryce@faradayrf.com>
2017-07-12 07:56:02 -0700

    First commit of faraday-bsl program
    Added setup.cfg entry points as well as skeleton application code.

---
+++ faraday/bootstrap.py
@@ -0,0 +1,97 @@
+#-------------------------------------------------------------------------------
+# Name:        /faraday/bootstrap.py
+# Purpose:     Bootstrap loads firmware onto the Faraday Wireless Node via USB
+#
+# Author:      Bryce Salmi
+#
+# Created:     7/12/2017
+# Licence:     GPLv3
+#-------------------------------------------------------------------------------
+
+import logging.config
+import threading
+import ConfigParser
+import socket
+import requests
+import os
+from time import sleep
+import sys
+import argparse
+import shutil
+
+# Start logging after importing modules
+relpath1 = os.path.join('etc', 'faraday')
+relpath2 = os.path.join('..', 'etc', 'faraday')
+setuppath = os.path.join(sys.prefix, 'etc', 'faraday')
+userpath = os.path.join(os.path.expanduser('~'), '.faraday')
+path = ''
+
+for location in os.curdir, relpath1, relpath2, setuppath, userpath:
+    try:
+        logging.config.fileConfig(os.path.join(location, "loggingConfig.ini"))
+        path = location
+        break
+    except ConfigParser.NoSectionError:
+        pass
+
+logger = logging.getLogger('BSL')
+
+#Create APRS configuration file path
+bslConfigPath = os.path.join(path, "bsl.ini")
+logger.debug('bsl.ini PATH: ' + bslConfigPath)
+
+bslConfig = ConfigParser.RawConfigParser()
+bslConfig.read(bslConfigPath)
+
+# Command line input
+parser = argparse.ArgumentParser(description='BSL will Boostrap load firmware onto Faraday via USB connection')
+parser.add_argument('--init-config', dest='init', action='store_true', help='Initialize BSL configuration file')
+parser.add_argument('--start', action='store_true', help='Start APRS server')
+
+# Parse the arguments
+args = parser.parse_args()
+
+def initializeBSLConfig():
+    '''
+    Initialize BSL configuration file from bsl.sample.ini
+
+    :return: None, exits program
+    '''
+
+    logger.info("Initializing BSL")
+    shutil.copy(os.path.join(path, "bsl.sample.ini"), os.path.join(path, "bsl.ini"))
+    logger.info("Initialization complete")
+    sys.exit(0)
+
+
+def configureBSL(args, bslConfigPath):
+    '''
+    Configure BSL configuration file from command line
+
+    :param args: argparse arguments
+    :param bslConfigPath: Path to bsl.ini file
+    :return: None
+    '''
+
+    config = ConfigParser.RawConfigParser()
+    config.read(os.path.join(path, "bsl.ini"))
+
+    # if args.callsign is not None:
+    #     config.set('APRSIS', 'CALLSIGN', args.callsign)
+
+    with open(bslConfigPath, 'wb') as configfile:
+        config.write(configfile)
+
+# Now act upon the command line arguments
+# Initialize and configure bsl
+if args.init:
+    initializeBSLConfig()
+configureBSL(args, bslConfigPath)
+
+# Read in BSL configuration parameters
+bslFile = bslConfig.read(bslConfigPath)
+
+# Check for --start option and exit if not present
+if not args.start:
+    logger.warning("--start option not present, exiting BSL server!")
+    sys.exit(0)
