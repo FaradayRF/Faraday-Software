7022e28f1a7a609cbe60d586cd6f344eaf2be43d
kb1lqc <bryce@faradayrf.com>
2017-07-18 23:50:18 -0700

    Updated createtiscript.py
    Removed commented out code, commented where appropriate, and generally
    cleaned up the file. Also added docstrings.

--- faraday/classes/createtiscript.py
+++ faraday/classes/createtiscript.py
@@ -1,33 +1,27 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# Name:        /faraday/classes/createtiscript.py
+# Purpose:     Texas Instruments bootloader script creation class
 #
-# Author:      Brent
+# Author:      Brenton Salmi, Bryce Salmi
 #
 # Created:     03/08/2016
-# Copyright:   (c) Brent 2016
-# Licence:     <your licence>
+# Licence:     GPLv3
 #-------------------------------------------------------------------------------
+
 import array
 import os
 import sys
 
-#filename = sys.argv[1]
-# filename = 'RF_Test_Firmware_1-17-17.txt'
-# device_com_port = 32
-#
-# f = open(filename, 'r')
-#
-# mem_addr_index = []
-# section_data_index = []
-# file_program_hex = f.read()
-
-#print "Arguement 0:", argument1
-
-
 class CreateTiBslScript(object):
+    '''
+    This class creates a TI Bootstrap Loader script file from a text file of Faraday firmware
+    '''
 
     def __init__(self, path, filename, comport, outputfilename, upgradeScript, logger):
+        """
+        initialize the class and variables
+        """
+
         self.path = path
         self._filename = os.path.join(os.path.expanduser('~'), '.faraday', 'firmware', filename)
         self.comport = comport
@@ -38,6 +32,12 @@ class CreateTiBslScript(object):
         self.logger = logger
 
     def createscript(self):
+        """
+        Create the TI Boostrap loader script
+
+        :return: None
+        """
+
         try:
             f = open(os.path.join(self.path,self._filename), 'r')
             file_program_hex = f.read()
@@ -52,26 +52,37 @@ class CreateTiBslScript(object):
         self.CreateOutputFile(self._outputfilename)
         self.CreateBslScript(self._upgradeScript)
 
-    # ParseTiTxtHexFile(file_program_hex)
-    # CreateOutputFile()
-    # CreateBslScript()
 
     def ParseTiTxtHexFile(self, input_file):
+        """
+        Parse the hex text file properly
+
+        :param input_file: File object to parse
+        :return: None
+        """
+
         datasections = input_file.split('@')
         datasections.pop(0)  #remove empty first index
 
-        #remove memory addresses and separate data in each section
+        # Remove memory addresses and separate data in each section
         for i in range(0, len(datasections)):
-            #Parse and strip TI-TXT format
+            # Parse and strip TI-TXT format
             a = datasections[i].replace('\n', '')
             a = a.replace(' ', '')
             mem_addr = str(a[0:4]).decode('hex')
             section_data = str(a[4:]).replace('q', '')  #remove end of file
-            #Ouput
+            # Output
             self.mem_addr_index.append(mem_addr)
             self.section_data_index.append(section_data.decode('hex'))
 
     def CalcTiTxtCrc16(self, databytes):
+        """
+        Check text file CRC16 value
+
+        :param databytes: data to perform CRC on
+        :return: CRC value
+        """
+
         data_array = array.array('B', databytes)
         chk = 0xffff
         for item in data_array:
@@ -85,6 +96,13 @@ class CreateTiBslScript(object):
     crc_script_index = []
 
     def CreateOutputFile(self, filename):
+        """
+        Create output TI bootstrap loader file from firmware text data
+
+        :param filename: Bootstrap loader output data filename
+        :return: CRC value
+        """
+
         textfile = open(os.path.join(self.path,filename), 'w')
         for i in range(0, len(self.mem_addr_index)):
             final_addr = self.mem_addr_index[i].encode('hex')
@@ -102,19 +120,19 @@ class CreateTiBslScript(object):
             textfile.writelines('\n\n')
 
     def CreateBslScript(self, filename):
-        #global device_com_port
-        #com_string = 'MODE 6xx UART 9600 COM%d PARITY' % device_com_port
+        """
+        Create output TI bootstrap loader script from firmware text data
+
+        :param filename: Bootstrap loader update script filename
+        :return: CRC value
+        """
+
         textfile = open(os.path.join(self.path,filename), 'w')
         textfile.writelines(("MODE 6xx UART 9600 ", str(self.comport), " PARITY", '\n'))
         textfile.writelines(("CHANGE_BAUD_RATE 115200", '\n'))
         textfile.writelines(("VERBOSE", '\n'))
         textfile.writelines(("RX_PASSWORD pass32_wrong.txt", '\n'))  #gives the wrong password to mass erase the memory
         textfile.writelines(("RX_PASSWORD pass32_default.txt", '\n'))
-        print "Script file:", self._filename
         textfile.writelines(("RX_DATA_BLOCK ", self._filename, '\n'))
         for i in range(0, len(self.crc_script_index)):
             textfile.writelines((str(self.crc_script_index[i]), '\n'))
-
-#ParseTiTxtHexFile(file_program_hex)
-#CreateOutputFile()
-#CreateBslScript()
